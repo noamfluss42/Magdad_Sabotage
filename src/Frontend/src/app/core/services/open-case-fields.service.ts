48502b5ad51a3c7e541cc9bc527b92abde496c7e
ronelgazar2019 <31697794+ronelgazar@users.noreply.github.com>
2021-12-23 14:28:17 +0200

    finished first screen implementation

--- src/Frontend/src/app/core/services/open-case-fields.service.ts
+++ src/Frontend/src/app/core/services/open-case-fields.service.ts
@@ -3,7 +3,6 @@ import { TextboxField } from '../utils/field-textbox';
 import { FormFieldBase } from '../utils/form-field-base';
 import { DropdownField } from '../utils/field-dropdown';
 //TODO: fetch fields from enum
-import { OpenCaseFields } from '../enums/open-case-fields';
 
 import { of } from 'rxjs';
 
@@ -15,49 +14,119 @@ export class OpenCaseFieldsService {
     const questions: FormFieldBase<string>[] = [
 
       new DropdownField({
+        key: 'referenceType',
+        label: 'סוג סימוכין',
+        options: [
+          {key: "פלא", value: "פלא"},
+        ]}),
+        new DropdownField({
+          key: 'eventType',
+          label: 'סוג אירוע',
+          options: [
+            {key: "פלילי", value: "פלילי"},
+            {key: 'פח"ע', value: 'פח"ע'},
+  
+          ]}),
+  
+  
+      new DropdownField({
         key: 'district',
         label: 'מחוז',
         options: [
-          {key: '',  value: ''},
-          {key: '1', value: 'מחוז 1'},
-          {key: '2', value: 'מחוז 2'},
-          {key: '3', value: 'מחוז 3'},
-        ],
+          {key: "מחוז צפון", value: "מחוז צפון"},
+          {key: "מחוז חוף", value: "מחוז חוף"},
+          {key: "מחוז מחוז", value: "מחוז מרכז"},
+          {key: "מחוז תל אביב", value: "מחוז תל אביב"},
+          {key: "מחוז ירושלים", value: "מחוז ירושלים"},
+          {key: ' מחוז ש"י ', value: 'מחוז ש"י'},
+          {key: ' מחוז דרום ', value: 'מחוז דרום'},
+        ]
       }),
-
+      // need data
       new DropdownField({
         key: 'area',
         label: 'מרחב',
         options: [
-          {key: '',  value: ''},
-          {key: '1', value: 'מרחב 1'},
-          {key: '2', value: 'מרחב 2'},
-          {key: '3', value: 'מרחב 3'},
-        ],
-      }),
-
+          {key: "מרחב צפון", value: "מרחב צפון"},
+  
+  
+        ]}),
       new DropdownField({
         key: 'station',
         label: 'תחנה',
         options: [
-          {key: '',  value: ''},
-          {key: '1', value: 'תחנה 1'},
-          {key: '2', value: 'תחנה 2'},
-          {key: '3', value: 'תחנה 3'},
-        ],
+          {key: "בין שאן", value: "בין שאן"},
+          {key: "טבריה", value: "טבריה"},
+          {key: "כנא", value: "כנא"},
+          {key: "מגדל העמק", value: "מגדל העמק"},
+          {key: "נצרת", value: "נצרת"},
+          {key: " נצרת עילית", value: "נצרת עילית"},
+          {key: " עפולה", value: "עפולה"},
+          {key: " שפרעם", value: "שפרעם"},
+    ]}),
+    new DropdownField({
+      key: 'InvestigatingUnit',
+      label: 'יחידת חקירות',
+      options: [
+        {key: "בין שאן", value: "בין שאן"},
+        {key: "טבריה", value: "טבריה"},
+        {key: "כנא", value: "כנא"},
+        {key: "מגדל העמק", value: "מגדל העמק"},
+        {key: "נצרת", value: "נצרת"},
+        {key: " נצרת עילית", value: "נצרת עילית"},
+        {key: " עפולה", value: "עפולה"},
+        {key: " שפרעם", value: "שפרעם"},
+  ]}),
+  
+    /*
+    במחוז צפון ישנם שלושה מרחבים: כינרת, עמקים וגליל. | במחוז דרום ישנם שלושה מרחבים: לכיש, נגב ואילת. | במחוז מרכז ישנם שלושה מרחבים: שרון, שפלה ונתב"ג. | במחוז ת"א ישנם ארבעה מרחבים: ירקון, דן, איילון ויפתח. | במחוז ש"י ישנם שני מרחבים: חברון ושומרון. | במחוז ירושלים ישנם שלושה מרחבים: דוד, קדם וציון. | במחוז חוף יש שני מרחבים: אשר ומנשה.
+    */
+    new TextboxField({
+        key: 'internalNumber',
+        label: 'מספר פנימי',
+        required: true,
+        type:'text'
       }),
-
-      new DropdownField({
-        key: 'unit',
-        label: 'יחידת חקירות',
-        options: [
-          {key: '',  value: ''},
-          {key: '1', value: 'יחידת חקירות 1'},
-          {key: '2', value: 'יחידת חקירות 2'},
-          {key: '3', value: 'יחידת חקירות 3'},
-        ],
+      
+  
+      new TextboxField({
+        key:"referenceNumber",
+        label:"מספר סימוכין",
+        required:true,
+        type:'text',
       }),
-
+      
+      
+      new TextboxField({
+        key: 'eventDate',
+        label: 'תאריך אירוע',
+        required: true,
+        type: 'text'
+      }),
+      new TextboxField({
+        key: 'ReceivedDate',
+        label : 'תאריך קבלה',
+        required: true,
+        type:'text'
+      }),
+      new TextboxField({
+        key: 'signDate',
+        label : 'תאריך קבלה',
+        required: true,
+        type:'text'
+      }),
+      new TextboxField({
+        key: "eventLocation",
+        label: "מקום האירוע",
+        required: true,
+        type: 'text'
+      }),
+      new TextboxField({
+        key: "eventDescription",
+        label: "תיאור האירוע",
+        required: true,
+        type: 'text'
+      })
     ];
 
     return of(questions.sort((a, b) => a.order - b.order));
