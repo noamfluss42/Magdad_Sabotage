975e9a57b1a464c82e4a9a55384287e97b318e8e
ofir <ofir.shklover@gmail.com>
2021-12-23 13:47:23 +0200

    basic backend server

---
+++ SuperAPI/caseHandler/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from caseHandler.models import Cases
+from caseHandler.serializers import CaseSerializer
+
+from django.core.files.storage import default_storage
+
+
+# Create your views here.
+@csrf_exempt
+def caseApi(request, case_name=""):
+    if request.method == 'GET':
+        cases = Cases.objects.all()
+        cases_serializer = CaseSerializer(cases, many=True)
+        return JsonResponse(cases_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        case_data = JSONParser().parse(request)
+        department_serializer = CaseSerializer(data=case_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
+            return JsonResponse("Added Successfully!!", safe=False)
+        return JsonResponse("Failed to Add.", safe=False)
+
+    elif request.method == 'PUT':
+        department_data = JSONParser().parse(request)
+        department = Cases.objects.get(DepartmentId=department_data['DepartmentId'])
+        department_serializer = CaseSerializer(department, data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update.", safe=False)
+
+    elif request.method == 'DELETE':
+        department = Cases.objects.get(CaseName=case_name)
+        department.delete()
+        return JsonResponse("Deleted Succeffully!!", safe=False)
