5d48596fd5ffc2ec9af199b75c00548189433fa2
ronelgazar2019 <31697794+ronelgazar@users.noreply.github.com>
2021-12-26 17:32:33 +0200

    added material ui support for ui design

--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -5,13 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DynamicFormFieldComponent } from './core/components/dynamic-form-field/dynamic-form-field.component';
 import { DynamicFormComponent } from './core/components/dynamic-form/dynamic-form.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
-  imports: [ BrowserModule, ReactiveFormsModule,HttpClientModule, ],
-  declarations: [ AppComponent, DynamicFormComponent, DynamicFormFieldComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  declarations: [AppComponent, DynamicFormComponent, DynamicFormFieldComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor() {
-  }
+  constructor() {}
 }
