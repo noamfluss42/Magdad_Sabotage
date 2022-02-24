bf54101ce57862838710d7c11945d15af645e12e
DanielBotnik <73408101+DanielBotnik@users.noreply.github.com>
2022-02-24 12:28:14 +0200

    revert changes

--- SuperAPI/caseHandler/models.py
+++ SuperAPI/caseHandler/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Case(models.Model):
-    reference_type = models.CharField(max_length=100)
-    event_type = models.CharField(max_length=100)
+    referenceType = models.CharField(max_length=100)
+    eventType = models.CharField(max_length=100)
     district = models.CharField(max_length=100)
     area = models.CharField(max_length=100)
     station = models.CharField(max_length=100)
-    investigating_unit = models.CharField(max_length=100)
-    internal_number = models.CharField(max_length=100, primary_key=True)
-    internal_number_year = models.CharField(max_length=100)
-    reference_number = models.CharField(max_length=100)
-    event_date = models.CharField(max_length=100)
-    received_date = models.CharField(max_length=100)
-    sign_date = models.CharField(max_length=100)
-    event_location = models.CharField(max_length=100)
-    event_description = models.CharField(max_length=100)
+    InvestigatingUnit = models.CharField(max_length=100)
+    internalNumber = models.CharField(max_length=100, primary_key=True)
+    internalNumberyear = models.CharField(max_length=100)
+    referenceNumber = models.CharField(max_length=100)
+    eventDate = models.CharField(max_length=100)
+    ReceivedDate = models.CharField(max_length=100)
+    signDate = models.CharField(max_length=100)
+    eventLocation = models.CharField(max_length=100)
+    eventDescription = models.CharField(max_length=100)
 
 class Exhibits(models.Model):
     exhibit_description = models.CharField(max_length=256)
